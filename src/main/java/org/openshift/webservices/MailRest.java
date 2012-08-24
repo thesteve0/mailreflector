82042c9c3e3087441404d9de399aeee15c432325
spousty <spousty@gadol>
2012-08-23 23:22:38 -0700

    try again

---
+++ src/main/java/org/openshift/webservices/MailRest.java
@@ -0,0 +1,17 @@
+package org.openshift.webservices;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+//Comment so we can deploy again
+
+@Path("/ws")
+public class MailRest {
+
+	@GET()
+	@Produces("text/plain")
+	public String sayHello() {
+	    return "Hello World!";
+	}
+}
